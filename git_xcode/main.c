143793e8242e78930668ba6ad69b7040c7f93118
ungsik yun <ungsik@server.wizplan.private>
2014-12-01 17:50:38 +0900

    Initial Commit

---
+++ git_xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  git_xcode
+//
+//  Created by ungsik yun on 2014. 12. 1..
+//  Copyright (c) 2014년 ungsik yun. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
